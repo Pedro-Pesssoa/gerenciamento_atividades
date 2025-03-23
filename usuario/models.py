a9f0289064207555c7f630d8589b4fee08918806
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2025-03-23 19:23:16 -0300

    alteração sistema de login

--- usuario/models.py
+++ usuario/models.py
@@ -4,10 +4,9 @@ from django.db import models
 
 class Usuario(AbstractUser):
     nome = models.CharField(max_length=150)
-    email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['nome', 'username']
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['nome']
 
     def __str__(self):
         return self.nome
